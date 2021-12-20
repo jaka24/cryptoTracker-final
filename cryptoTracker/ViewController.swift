84a4bc990a59bfec7a31df8d53f9c06355b3d623
John-marc Attis <attisjohn@yahoo.com>
2021-12-20 01:32:53 -0500

    class exit 24

--- cryptoTracker/ViewController.swift
+++ cryptoTracker/ViewController.swift
@@ -5,15 +5,82 @@
 //  Created by John-Marc Attis on 12/19/21.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        if let symbol = textField.text {
+            
+            getData(symbol : symbol)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
+    
+    var url = "https://min-api.cryptocompare.com/data/price?tsyms=USD"
+    
+    func getData(symbol : String) {
+        
+        
+        url = "\(url)&fsym=\(symbol)"
+        
+        guard let url = URL(string: url) else {return}
+        
+        
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            
+            
+            
+            guard let data = data, error == nil else {return}
+            
+            print("Data Recieved")
+            
+            do {
+                
+            let Result = try JSONDecoder().decode(APIResponse.self, from: data)
+                
+            print(Result.USD)
+                
+                
+                
+                DispatchQueue.main.async{
+                    
+                
+                self.outputLabel.text = "\(Result.USD)"
+                }
+            }
+            catch{
+                
+                print(error.localizedDescription)
+                
+            }
+        
 
-
+        }
+        
+        
+        task.resume()
+        
+                
+    }
+    
+    struct APIResponse : Codable {
+        
+        let USD : Float
+    }
 }
 
